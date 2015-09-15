6568bf29361b7dfff6138c82ce2ef14871ef5c65
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-09-15 12:03:24 +0300

    First scene and some UI stuff

---
+++ WeaknessFantasy/Assets/Scripts/MouseController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		print ("Mouse clicked");
+		Debug.Log ("Tööt");
+	}
+}
