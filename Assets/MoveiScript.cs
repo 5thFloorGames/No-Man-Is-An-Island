885fa624c711b0b6822a15a1222bbec7f386507d
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-09-18 14:27:45 +0300

    adding rest of the scenes

---
+++ Assets/MoveiScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoveiScript : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		print ("starting movie");
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
